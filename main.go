1429dc48c69c9d990bdd398e0f1ca8c85df1ccaf
nukecoke1828 <nukecoke1828@users.noreply.github.com>
2026-05-17 17:16:47 +0800

    Add CLI downloader implementation

--- main.go
+++ main.go
@@ -1,246 +1,270 @@
 ﻿package main
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 	"sync"
 
-	"os"
-	"strconv"
 	"time"
 )
 
-var defaultDownloadPath = "C:\\Users\\imdxh\\Downloads"
+var DefaultDownloadPath = "C:\\Users\\imdxh\\Downloads"
 var ch chan string = make(chan string)
-var bucket chan interface{} = make(chan interface{}, 5)
+var IDs sync.Map
+var Retrychan chan string = make(chan string)
+var MaxID int = 0
+var Limiter chan struct{} = make(chan struct{}, 5)
 var retryLimit sync.Map
-var retrych chan string = make(chan string)
-var waitdownloadCh chan Request = make(chan Request, 10)
+var waitqueue chan Request = make(chan Request, 5)
 
-type Request struct {
-	resp http.ResponseWriter
-	req  *http.Request
+type Record struct {
+	ID       int    `json:"ID"`
+	Status   string `json:"Status"`
+	URL      string `json:"URL"`
+	Filename string `json:"Filename"`
 }
 
-func DownloadFile(url string, ch chan<- string, filename string, nowTime time.Time) (err error) {
+type Request struct {
+	URL      string
+	Filename string
+	id       int
+	flag     bool
+}
+
+func DownloadFile(url string, filename string, id int, flag bool) (err error) {
+	HandleStatus("下载中...", id, flag, url, filename)
 	var Done chan string = make(chan string)
 	go func() {
-		ticker := time.NewTicker(10 * time.Second)
-		timer := time.NewTimer(5 * time.Hour) // 设置一个较长的超时时间，实际应用中应该根据需求调整,这里设置为5小时，避免过早超时
+		timer := time.NewTimer(5 * time.Hour)
 		defer timer.Stop()
-		defer ticker.Stop()
-		for {
+		select {
+		case <-timer.C:
+			HandleStatus("下载超时", id, flag, url, filename)
+			return
+		case <-Done:
 			select {
-			case result := <-Done:
-				if (result == "文件下载成功") || (result == "下载文件失败") {
-					if result == "下载文件失败" {
-						if count, exists := retryLimit.Load(filename); !exists || (exists && count.(int) < 3) {
-							retrych <- url + "," + filename // 将需要重试的URL和文件名发送到重试通道
-							go func(count int) {
-								time.Sleep(time.Second * 1)
-								if count_nuevo, _ := retryLimit.Load(filename); count_nuevo == count {
-									<-retrych                               // 用户30秒内没有重试，自动放弃重试机会
-									timer := time.NewTimer(5 * time.Minute) // 过段时间再重置重试次数避免用户短时间内重试过多次
-									defer timer.Stop()
-									<-timer.C
-									retryLimit.Delete(filename) // 放弃重试机会，重置重试次数记录
-								}
-							}(count.(int))
-						} else {
-							go func() {
-								timer := time.NewTimer(5 * time.Minute) // 过段时间再重置重试次数避免用户短时间内重试过多次
-								defer timer.Stop()
-								<-timer.C
-								retryLimit.Delete(filename) // 重试次数超过限制，重置重试次数记录
-							}()
-						}
-					}
-					go func() {
-						timer := time.NewTimer(5 * time.Second) // 稍微等待一下确保限流器已经释放桶位
-						defer timer.Stop()
-						<-timer.C
-						select {
-						case request := <-waitdownloadCh: // 如果有等待下载的请求，尝试处理下一个下载请求
-							q := request.req.URL.Query()
-							q.Add("wait", "yes")                  // 添加参数
-							request.req.URL.RawQuery = q.Encode() // 重新编码回 RawQuery
-							ch <- "已处理等待队列中的下载请求"
-							DownloadFileHandler().ServeHTTP(request.resp, request.req)
-						default:
-							ch <- "当前没有等待下载的请求"
-						}
-					}()
-					return
-				}
-			case <-ticker.C:
-				ch <- "文件较大或网络较慢，下载仍在进行中..."
-			case <-timer.C:
-				ch <- "下载文件超时，可能网络较慢或文件过大，请稍后再试"
-				Done <- "下载文件失败"
-				return
+			case req := <-waitqueue:
+				Limiter <- struct{}{}
+				go DownloadFile(req.URL, req.Filename, req.id, req.flag)
+			default:
 			}
+			return
 		}
 	}()
-	if count, exists := retryLimit.Load(filename); exists {
-		count = count.(int) + 1
-		retryLimit.Store(filename, count)
-		if count.(int) > 3 {
-			ch <- "下载文件失败，重试次数超过限制"
-			Done <- "下载文件失败"
-			return errors.New("下载文件失败，重试次数超过限制")
+	if count, exist := retryLimit.Load(url); exist {
+		fmt.Printf("重试次数为%d", count)
+		if count.(int) >= 3 {
+			fmt.Printf("重试太多了！！！")
+			HandleStatus("下载失败，超过重试次数,请5分钟后重试", id, flag, url, filename)
+			go func() {
+				timer := time.NewTimer(5 * time.Second) // 5分钟后重置重试次数,避免一直重试
+				<-timer.C
+				retryLimit.Delete(url)
+			}()
+			<-Limiter
+			return nil
+		} else {
+			retryLimit.Store(url, count.(int)+1)
 		}
 	} else {
-		retryLimit.Store(filename, 1)
+		retryLimit.Store(url, 1)
 	}
-	resp, err := http.Get(url)
+	resp := &http.Response{}
+	resp, err = http.Get(url)
 	if err != nil {
-		ch <- "下载文件失败" + err.Error()
-		Done <- "下载文件失败"
+		ch <- err.Error()
+		Done <- "下载失败"
+		<-Limiter
+		HandleStatus("下载失败", id, flag, url, filename)
 		return err
 	}
 	defer resp.Body.Close()
-	data, err := os.Create(defaultDownloadPath + "\\" + filename)
+	data := &os.File{}
+	data, err = os.Create(DefaultDownloadPath + "\\" + filename)
 	if err != nil {
-		ch <- "下载文件失败" + err.Error()
-		Done <- "下载文件失败"
+		ch <- err.Error()
+		Done <- "下载失败"
+		<-Limiter
+		HandleStatus("下载失败", id, flag, url, filename)
 		return err
 	}
 	defer data.Close()
 	_, err = io.Copy(data, resp.Body)
 	if err != nil {
-		ch <- "下载文件失败" + err.Error()
-		Done <- "下载文件失败"
+		ch <- err.Error()
+		Done <- "下载失败"
+		<-Limiter
+		HandleStatus("下载失败", id, flag, url, filename)
 		return err
 	}
-	length, err := os.Stat(defaultDownloadPath + "\\" + filename)
+	nowTime := time.Now()
+	length, err := os.Stat(DefaultDownloadPath + "\\" + filename)
 	if err != nil {
-		ch <- "获取文件大小失败" + err.Error()
-		Done <- "下载文件失败"
+		ch <- err.Error()
+		Done <- "下载失败"
+		<-Limiter
+		HandleStatus("下载失败", id, flag, url, filename)
 		return err
 	}
-	ch <- "文件下载成功" + ",文件大小为" + strconv.Itoa(int(length.Size())) + "字节,下载耗时" + time.Since(nowTime).String() + "，文件来源URL：" + url
-	Done <- "文件下载成功"
+	HandleStatus("文件下载成功"+",文件大小为"+strconv.Itoa(int(length.Size()))+"字节,下载耗时"+time.Since(nowTime).String()+"，文件来源URL："+url, id, flag, url, filename)
+	ch <- "下载完成"
+	Done <- "下载完成"
+	<-Limiter
 	return nil
 }
 
-func DownloadFileHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		url := r.URL.Query().Get("url")
-		filename := r.URL.Query().Get("filename")
-		wait := r.URL.Query().Get("wait") // 避免将等待中的请求加入等待队列
+func HandleDownload() {
+	var url string
+	var filename string
+	for {
+		var flag string
+		fmt.Println("输入yes开始下载，输入list查看历史下载记录，输入quit退出")
+		fmt.Scan(&flag)
+		if flag == "quit" {
+			break
+		} else if flag == "list" {
+			HandleList()
+			continue
+		}
+		fmt.Printf("请输入下载地址和保存的文件名")
+		_, err := fmt.Scan(&url, &filename)
+		if err != nil {
+			fmt.Println("输入错误，请重新输入")
+			continue
+		}
 		if url == "" {
-			http.Error(w, "URL参数不能为空", http.StatusBadRequest)
-			return
+			fmt.Println("下载地址不能为空，请重新输入")
+			continue
 		}
 		if filename == "" {
-			filename = "downloaded_file_" + strconv.FormatInt(time.Now().Unix(), 10) // 如果没有提供文件名，使用默认命名方式
+			filename = url[strings.LastIndex(url, "/")+1:] // 获取url最后一个/后面的字符串
 		}
-		nowTime := time.Now()
-		if len(waitdownloadCh) != 0 && wait != "yes" { // 优先处理等待下载的请求，当前有等待下载的请求时，新的下载请求直接加入等待队列
-			if len(waitdownloadCh) == 10 {
-				http.Error(w, "下载请求过多，请稍后再试", http.StatusTooManyRequests)
-				return
-			}
-			lock := sync.Mutex{}
-			lock.Lock()
-			waitdownloadCh <- Request{resp: w, req: r} // 将请求加入等待队列
-			w.Write([]byte("当前有等待下载的请求，已将您的请求加入等待队列，稍后开始下载"))
-			lock.Unlock()
-			return
-		}
+		MaxID++
+		id := MaxID
+		_, Iflag := IDs.Load(id)
 		select {
-		case bucket <- struct{}{}: // 尝试占用一个桶位，限制并发数量
-		default: // 如果桶位已满，则将请求加入等待队列
-			if len(waitdownloadCh) == 10 {
-				http.Error(w, "下载请求过多，请稍后再试", http.StatusTooManyRequests)
-				return
+		case Limiter <- struct{}{}:
+		default:
+			if len(waitqueue) == 5 {
+				HandleStatus("下载请求太多了请稍候再试", id, Iflag, url, filename)
+				continue
 			}
-			lock := sync.Mutex{}
-			lock.Lock()
-			waitdownloadCh <- Request{resp: w, req: r} // 将请求加入等待队列
-			w.Write([]byte("下载请求过多，已将您的请求加入等待队列，稍后开始下载"))
-			lock.Unlock()
-			return
+			waitqueue <- Request{url, filename, MaxID, false}
+			HandleStatus("下载文件过多已将文件加入等待队列，稍后开始下载", id, Iflag, url, filename)
+			continue
 		}
-		err := DownloadFile(url, ch, filename, nowTime)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			<-bucket
-		} else {
-			w.Write([]byte("文件下载成功"))
-			w.WriteHeader(http.StatusOK)
-			<-bucket
-		}
+		go func() {
+			IDs.Store(id, url+","+filename)
+			HandleStatus("开始下载", id, Iflag, url, filename)
+			Iflag = true
+			_ = DownloadFile(url, filename, id, Iflag)
+		}()
 	}
 }
 
-func RetryDownloadFileHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		option := r.URL.Query().Get("option")
-		msg := <-retrych // 从重试通道接收重试信息
-		parts := strings.SplitN(msg, ",", 2)
-		if len(parts) != 2 {
-			http.Error(w, errors.New("无效的重试信息").Error(), http.StatusBadRequest)
-			return
-		}
-		url := parts[0]
-		filename := parts[1]
-		nowTime := time.Now()
-		if option == "no" {
-			go func() {
-				timer := time.NewTimer(5 * time.Minute) // 过段时间再重置重试次数避免用户短时间内重试过多次
-				defer timer.Stop()
-				<-timer.C
-				retryLimit.Delete(filename) // 放弃重试机会，重置重试次数记录
-			}()
-			http.Error(w, errors.New("用户放弃下载").Error(), http.StatusInternalServerError)
-			return
-		}
-		bucket <- struct{}{} // 占用一个桶位，限制并发数量
-		err := DownloadFile(url, ch, filename, nowTime)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			<-bucket
-		} else {
-			w.Write([]byte("文件下载成功"))
-			w.WriteHeader(http.StatusOK)
-			<-bucket
-		}
+func init() {
+	data, err := os.ReadFile("history.json")
+	if err != nil {
+		panic(err)
+	}
+	var records []Record
+	json.Unmarshal(data, &records)
+	if len(records) > 0 {
+		MaxID = records[len(records)-1].ID
+	}
+	for _, record := range records {
+		IDs.Store(record.ID, record.URL+","+record.Filename)
 	}
 }
 
-// 用于在尝试超过重试次数限制后重置全部重试次数记录，用于用户如果在5分钟内都没有执行任何下载操作，则自动重置重试次数记录，
-// 允许用户重新尝试下载，使用方法使用goroutine监听下载通道，如果5分钟内没有收到下载请求，则自动重置重试次数记录，具体使用代码不做演示
-func ResetRetryLimit(dlownloadCh <-chan string) {
-	// 创建一个5分钟的定时器
-	timer := time.NewTimer(5 * time.Minute)
-	// 确保函数退出时停止定时器
-	defer timer.Stop()
-	// 无限循环，持续监听定时器和下载通道
-	for {
-		select {
-		// 定时器触发的情况
-		case <-timer.C:
-			retryLimit = sync.Map{} // 重置重试次数记录
-			log.Printf("重试次数记录已重置，用户可以重新尝试下载")
-			timer.Reset(5 * time.Minute) // 重置定时器，继续监听下一轮
-		case <-dlownloadCh: // 收到下载请求，重置定时器，继续监听下一轮
-			timer.Reset(5 * time.Minute)
+func HandleStatus(status string, id int, flag bool, url string, filename string) {
+	var mu sync.Mutex
+	mu.Lock()
+	defer mu.Unlock()
+	data, err := os.ReadFile("history.json")
+	if err != nil {
+		panic(err)
+	}
+	var records []Record
+	if len(data) > 0 {
+		err = json.Unmarshal(data, &records)
+		if err != nil {
+			panic(err)
+		}
+	}
+	if !flag {
+		records = append(records, Record{id, status, url, filename})
+	} else {
+		for i, record := range records {
+			if record.ID == id {
+				records[i].Status = status
+				break
+			}
+		}
+	}
+	data, err = json.MarshalIndent(records, "", "")
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("history.json", data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func HandleRetry(url string, filename string, id int, flag bool) {
+	Limiter <- struct{}{}
+	go func() {
+		fmt.Printf("开始重试下载文件:%s\n", filename)
+		err := DownloadFile(url, filename, id, flag)
+		fmt.Printf("重试结束!!!")
+		if err != nil {
+			HandleStatus("重试失败", id, flag, url, filename)
+		}
+	}()
+}
+
+func HandleList() {
+	data, err := os.ReadFile("history.json")
+	if err != nil {
+		panic(err)
+	}
+	var records []Record
+	err = json.Unmarshal(data, &records)
+	if err != nil {
+		panic(err)
+	}
+	for _, record := range records {
+		fmt.Printf("ID: %d, Status: %s\n", record.ID, record.Status)
+	}
+	var id string
+	fmt.Println("输入ID重试下载，输入quit退出")
+	fmt.Scan(&id)
+	if id == "quit" {
+		return
+	} else {
+		Uid, _ := strconv.Atoi(id)
+		// fmt.Println(Uid)
+		if Rid, exist := IDs.Load(Uid); exist {
+			urlAndfilename := strings.SplitN(Rid.(string), ",", 2)
+			HandleRetry(urlAndfilename[0], urlAndfilename[1], Uid, true)
 		}
 	}
 }
 
 func main() {
-	http.HandleFunc("/download", DownloadFileHandler())
-	http.HandleFunc("/retry", RetryDownloadFileHandler())
 	go func() {
+		file, _ := os.OpenFile("Log.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		defer file.Close()
 		for msg := range ch {
-			log.Printf("日志: %s", msg)
+			_, _ = file.WriteString(msg + "\n")
 		}
 		defer close(ch)
 	}()
-	http.ListenAndServe(":8080", nil)
+	HandleDownload()
 }
--- main_test.go
+++ main_test.go
@@ -1,127 +1,138 @@
-package main
+﻿package main
 
 import (
-	"log"
-	"net/http/httptest"
+	"bufio"
+	"encoding/json"
 	"os"
-	"strconv"
-	"sync"
 	"testing"
+	"time"
 )
 
-var TestURL []string = []string{
-	"https://picsum.photos/seed/guoxue1/400/300.jpg&filename=testphoto1.jpg",
-	"https://picsum.photos/seed/zhonghua2/400/300.jpg&filename=testphoto2.jpg",
-	"https://picsum.photos/seed/poet3/400/300.jpg&filename=testphoto3.jpg",
-	"https://picsum.photos/seed/happiness4/400/300.jpg&filename=testphoto4.jpg",
-	"https://picsum.photos/seed/western5/400/300.jpg&filename=testphoto5.jpg",
-	"https://picsum.photos/seed/compare6/400/300.jpg&filename=testphoto6.jpg",
-	"https://picsum.photos/seed/poetry7/400/300.jpg&filename=testphoto7.jpg",
-	"fakeurl",
-	"https://picsum.photos/seed/guoxue1/400/300.jpg&filename=testphoto1.jpg",
-	"https://picsum.photos/seed/guoxue1/400/300.jpg&filename=testphoto1.jpg",
-	"https://picsum.photos/seed/guoxue1/400/300.jpg&filename=testphoto1.jpg",
-	"https://picsum.photos/seed/guoxue1/400/300.jpg&filename=testphoto1.jpg",
-}
-
-var TestReq []Req = []Req{
-	{"https://picsum.photos/seed/guoxue1/400/300.jpg,testphoto1.jpg", "yes"},
-	{"https://picsum.photos/seed/guoxue1/400/300.jpg,testphoto1.jpg", "no"},
-	{"wrongurltestphoto1.jpg", "yes"},
-	{"retry,fail", "yes"},
-}
-
-type Req struct {
-	req    string
-	status string
-}
-
-func TestDownloadFileHandler(t *testing.T) {
+func TestDownloadFile(t *testing.T) {
 	ch = make(chan string)
-	bucket = make(chan interface{}, 5)
-	retrych = make(chan string)
-	waitdownloadCh = make(chan Request, 10)
-	go func() { // 消化日志通道中的日志，避免阻塞导致测试流程卡死
+	Limiter = make(chan struct{}, 5)
+	go func() {
+		file, _ := os.OpenFile("Log.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		defer file.Close()
 		for msg := range ch {
-			log.Printf("日志: %s", msg)
+			_, _ = file.WriteString(msg + "\n")
 		}
 		defer close(ch)
 	}()
-	for idx, url := range TestURL {
-		resp := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/download?url="+url, nil)
-		if idx == 5 {
-			for i := 0; i < 5; i++ {
-				bucket <- struct{}{} // 模拟下载队列已满
+	err := os.Truncate("Log.log", 0)
+	if err != nil {
+		t.Errorf("Error truncating file: %v", err)
+	}
+	err = os.Truncate("history.json", 0)
+	if err != nil {
+		t.Errorf("Error truncating file: %v", err)
+	}
+	file, err := os.Open("Log.log")
+	if err != nil {
+		t.Errorf("Error opening file: %v", err)
+	}
+	defer file.Close()
+	Limiter <- struct{}{}
+	HandleStatus("开始下载", 0, false, "https://picsum.photos/seed/guoxue1/400/300.jpg", "test1")
+	_ = DownloadFile("https://picsum.photos/seed/guoxue1/400/300.jpg", "test1", 0, true)
+	Limiter <- struct{}{}
+	HandleStatus("开始下载", 1, false, "xxx", "test2")
+	_ = DownloadFile("xxx", "test2", 1, true)
+	data, err := os.ReadFile("history.json")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+	var Records []Record
+	var Logs []string
+	err = json.Unmarshal(data, &Records)
+	if err != nil {
+		t.Errorf("Error unmarshalling JSON: %v", err)
+	}
+	for _, record := range Records {
+		if record.ID == 0 {
+			var status []rune = []rune(record.Status)
+			if string(status[:6]) != "文件下载成功" {
+				t.Errorf("Expected got '文件下载成功' but got %s", record.Status)
+			}
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				Logs = append(Logs, scanner.Text()) // 获取当前行（不含换行符）
+			}
+			if Logs[record.ID] != "下载完成" {
+				t.Errorf("Expected got '下载完成' but got %s", Logs[record.ID])
 			}
 		}
-		if idx == 7 {
-			// 恢复下载队列，以便测试下一个请求
-			bucket = make(chan interface{}, 5)
-			waitdownloadCh = make(chan Request, 10)
-		}
-		DownloadFileHandler().ServeHTTP(resp, req)
-		if idx < 5 {
-			if resp.Code != 200 || resp.Body.String() != "文件下载成功" {
-				t.Errorf("DownloadFileHandler failed to download file %s", url)
+		if record.ID == 1 {
+			var status []rune = []rune(record.Status)
+			if string(status[:4]) != "下载失败" {
+				t.Errorf("Expected got 下载失败 but got %s", record.Status)
 			}
-			_, err := os.ReadFile(defaultDownloadPath + "\\testphoto" + strconv.Itoa(idx+1) + ".jpg") // 检查文件是否生成
-			if err != nil {
-				t.Errorf("DownloadFileHandler failed to find downloaded file %s", url)
+			scanner := bufio.NewScanner(file)
+			for scanner.Scan() {
+				Logs = append(Logs, scanner.Text()) // 获取当前行（不含换行符）
 			}
-		}
-		if idx == 5 {
-			if resp.Body.String() != "下载请求过多，已将您的请求加入等待队列，稍后开始下载" {
-				t.Errorf("Expected got 下载请求过多，已将您的请求加入等待队列，稍后开始下载 but got %s", resp.Body.String())
-			}
-		}
-		if idx == 6 {
-			if resp.Body.String() != "当前有等待下载的请求，已将您的请求加入等待队列，稍后开始下载" {
-				t.Errorf("Expected got 当前有等待下载的请求，已将您的请求加入等待队列，稍后开始下载 but got %s", resp.Body.String())
-			}
-		}
-		if idx == 7 {
-			if resp.Body.String() != "Get \"fakeurl\": unsupported protocol scheme \"\"\n" {
-				t.Errorf("Expected got Get \"fakeurl\": unsupported protocol scheme \"\"\n but got %s", resp.Body.String())
-			}
-			retryLimit = sync.Map{} // 重置重试次数记录，避免影响后续测试
-		}
-		if idx == 11 {
-			if resp.Body.String() != "下载文件失败，重试次数超过限制\n" || resp.Code != 500 {
-				t.Errorf("Expected got 下载文件失败，重试次数超过限制\n but got %s", resp.Body.String())
+			if Logs[record.ID] != "Get \"xxx\": unsupported protocol scheme \"\"" {
+				t.Errorf("Expected got 'Get \"xxx\": unsupported protocol scheme \"\"' but got %s", Logs[record.ID])
 			}
 		}
 	}
 }
 
-func TestRetryDownloadFileHandler(t *testing.T) {
+func TestRetry(t *testing.T) {
 	ch = make(chan string)
-	bucket = make(chan interface{}, 5)
-	retrych = make(chan string)
+	Limiter = make(chan struct{}, 5)
 	go func() {
+		file, _ := os.OpenFile("Log.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		defer file.Close()
 		for msg := range ch {
-			log.Printf("日志: %s", msg)
+			_, _ = file.WriteString(msg + "\n")
 		}
 		defer close(ch)
 	}()
-	for idx, req := range TestReq {
-		go func() { // 异步发送避免阻塞测试流程
-			retrych <- req.req // 模拟一个需要重试的下载请求
-		}()
-		resp := httptest.NewRecorder()
-		req := httptest.NewRequest("GET", "/retry?option="+req.status, nil)
-		RetryDownloadFileHandler().ServeHTTP(resp, req)
-		if idx == 0 && (resp.Body.String() != "文件下载成功" || resp.Code != 200) {
-			t.Errorf("Expected got 文件下载成功 but got %s", resp.Body.String())
-		}
-		if idx == 1 && (resp.Body.String() != "用户放弃下载\n" || resp.Code != 500) {
-			t.Errorf("Expected got 用户放弃下载\n but got %s", resp.Body.String())
-		}
-		if idx == 2 && (resp.Body.String() != "无效的重试信息\n" || resp.Code != 400) {
-			t.Errorf("Expected got 无效的重试信息\n but got %s", resp.Body.String())
-		}
-		if idx == 3 && (resp.Body.String() != "Get \"retry\": unsupported protocol scheme \"\"\n" || resp.Code != 500) {
-			t.Errorf("Expected got Get \"retry\": unsupported protocol scheme \"\"\n but got %s", resp.Body.String())
-		}
+
+	HandleRetry("xxx", "test2", 1, true)
+	time.Sleep(5 * time.Second)
+	data, err := os.ReadFile("history.json")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+	var Records []Record
+	err = json.Unmarshal(data, &Records)
+	if err != nil {
+		t.Errorf("Error unmarshalling JSON: %v", err)
+	}
+	if Records[1].Status != "重试失败" {
+		t.Errorf("Expected got 重试失败 but got %s", Records[1].Status)
+	}
+	HandleRetry("xxx", "test2", 1, true)
+	time.Sleep(2 * time.Second)
+	HandleRetry("xxx", "test2", 1, true)
+	time.Sleep(2 * time.Second)
+	HandleRetry("xxx", "test2", 1, true)
+	time.Sleep(2 * time.Second)
+	data, err = os.ReadFile("history.json")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+	err = json.Unmarshal(data, &Records)
+	if err != nil {
+		t.Errorf("Error unmarshalling JSON: %v", err)
+	}
+	if Records[1].Status != "下载失败，超过重试次数,请5分钟后重试" {
+		t.Errorf("Expected got 下载失败，超过重试次数,请5分钟后重试 but got %s", Records[1].Status)
+	}
+	time.Sleep(5 * time.Second) // 需要测试时要将重置时间改为5秒
+	HandleRetry("xxx", "test2", 1, true)
+	time.Sleep(2 * time.Second)
+	data, err = os.ReadFile("history.json")
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+	err = json.Unmarshal(data, &Records)
+	if err != nil {
+		t.Errorf("Error unmarshalling JSON: %v", err)
+	}
+	if Records[1].Status != "重试失败" {
+		t.Errorf("Expected got 重试失败 but got %s", Records[1].Status)
 	}
 }
